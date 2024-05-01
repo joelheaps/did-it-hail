6c06569913514caf07ca5f100544a73595c93246
Joel Heaps <joel@joelheaps.com>
2024-04-30 23:16:48 -0500

    Lint utils

--- src/did_it_hail/utils.py
+++ src/did_it_hail/utils.py
@@ -1,8 +1,11 @@
-from typing import Iterator
+"""Module containing utility functions for other modules."""
+
+from collections.abc import Iterator
 from pathlib import Path
 
 
 def file_order_generator() -> Iterator[str]:
+    """Generate a three-letter string in alphabetical order."""
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     for first in alphabet:
         for second in alphabet:
@@ -11,6 +14,7 @@ def file_order_generator() -> Iterator[str]:
 
 
 def clear_dir(directory: Path) -> None:
+    """Remove all files in the given directory."""
     if directory.exists():
         for file in directory.rglob("*"):
             if file.is_file():
