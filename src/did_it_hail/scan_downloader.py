8b5b01f7297ac2a6874b5879f1c4d2a2158b54fe
Joel Heaps <joel@joelheaps.com>
2024-04-30 23:15:04 -0500

    Lint scan_downloader.py

--- src/did_it_hail/scan_downloader.py
+++ src/did_it_hail/scan_downloader.py
@@ -1,50 +1,51 @@
+"""Downloads radar scans from the NOAA FTP server and converts them to netCDF files."""
+
 import ftplib
+import time
+from datetime import datetime
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-import xarray as xr
+
 import numpy as np
-from metpy.io import Level3File
+import xarray as xr
 from metpy.calc import azimuth_range_to_lat_lon
-from datetime import datetime
-import cartopy.crs as ccrs
-import matplotlib.pyplot as plt
+from metpy.io import Level3File
 from metpy.units import units
-import time
 
 FTP_URL: str = "tgftp.nws.noaa.gov"
 PRODUCT_PATH: str = "/SL.us008001/DF.of/DC.radar/DS.165h0/SI.koax"
 DEST_DIR: Path = Path("download_cache")
 
 
-class FtpRadarDownloader:
+class _FtpRadarDownloader:
     latest_filename: str = "sn.last"
 
-    def __init__(self, ftp_url: str, product_path: str):
+    def __init__(self, ftp_url: str, product_path: str) -> None:
         self.ftp_url = ftp_url
         self.product_path = product_path
-        self.ftp = ftplib.FTP(self.ftp_url)
+        self.ftp = ftplib.FTP(self.ftp_url)  # noqa: S321
         self.ftp.login()
         self.ftp.cwd(self.product_path)
 
     def get_latest_radar_file(self, output_file: Path) -> None:
-        "Write the latest radar file to output_file"
+        """Write the latest radar file to output_file."""
         with output_file.open("wb") as f:
             self.ftp.retrbinary(f"RETR {self.latest_filename}", f.write)
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.ftp.quit()
 
 
 def get_azimuth_midpoints(
-    start_azimuths: np.ndarray, end_azimuths: np.ndarray
+    start_azimuths: np.ndarray,
+    end_azimuths: np.ndarray,
 ) -> np.ndarray:
-    """
-    Average start_azimuths[n] and end_azimuths[n] to get the midpoint azimuth.
-    """
+    """Average start_azimuths[n] and end_azimuths[n] to get the midpoint azimuth."""
     return (start_azimuths + end_azimuths) / 2
 
 
 def get_da_from_scan(radar_file: Path) -> xr.DataArray:
+    """Convert radar scan data to a DataArray."""
     l3f: Level3File = Level3File(radar_file)
     payload: dict = l3f.sym_block[0][0]
     product_time: datetime = l3f.metadata["prod_time"]
@@ -55,7 +56,8 @@ def get_da_from_scan(radar_file: Path) -> xr.DataArray:
     range_steps: int = data.shape[-1]  # Probably 1200
     ranges = units.Quantity(np.linspace(0, l3f.max_range, range_steps), "kilometers")
     averaged_azimuths = get_azimuth_midpoints(
-        np.array(payload["start_az"]), np.array(payload["end_az"])
+        np.array(payload["start_az"]),
+        np.array(payload["end_az"]),
     )
     azimuths = units.Quantity(averaged_azimuths, "degrees")
 
@@ -87,8 +89,9 @@ def get_da_from_scan(radar_file: Path) -> xr.DataArray:
     return da
 
 
-def download_and_convert_last_radar_scan():
-    ftp_downloader = FtpRadarDownloader(FTP_URL, PRODUCT_PATH)
+def download_and_convert_last_radar_scan() -> None:
+    """Download and convert the latest radar scan to a netcdf file."""
+    ftp_downloader = _FtpRadarDownloader(FTP_URL, PRODUCT_PATH)
     with NamedTemporaryFile() as tmp_file:
         ftp_downloader.get_latest_radar_file(Path(tmp_file.name))
         da = get_da_from_scan(Path(tmp_file.name))
@@ -98,12 +101,13 @@ def download_and_convert_last_radar_scan():
     da.to_netcdf(DEST_DIR / f"{da.name}.nc")
 
 
-def main():
+def main() -> None:
+    """Download the latest radar scan and save it to a netcdf file, in a loop."""
     # Every 120s, download the latest radar scan and save it to a netcdf file
     while True:
         try:
             download_and_convert_last_radar_scan()
-        except Exception as e:
+        except Exception as e:  # noqa: BLE001
             print(f"Error: {e}")
         print("Sleeping for 120s...")
         time.sleep(120)
